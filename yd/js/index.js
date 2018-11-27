dc586b739a629b65615e0271a43384cbef71eb92
duanhb <trefoil.duan@ibreakingpoint.com>
2018-11-27 16:52:16 +0800

    修改文件路径

--- yd/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    //菜单点击
-    $(".J_menuItem").on('click',function(){
-        var url = $(this).attr('href');
-        $("#iframepage").attr('src',url);
-        return false;
-    });
-});
